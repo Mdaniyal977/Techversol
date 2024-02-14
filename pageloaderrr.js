b3916f985d8723e4560152fe5989ed8970c089d5
Mdaniyal977 <daniyalmuhammad977@gmail.com>
2024-02-14 20:45:57 +0500

    xZXZ

---
+++ pageloaderrr.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Hide loader once content is loaded
+    var loaderWrapper = document.querySelector('.page-loader');
+    loaderWrapper.style.display = 'none';
+  });
